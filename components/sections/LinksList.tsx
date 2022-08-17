1549274d2d11b74e5d5f9916074335fd7c26359f
Andrew Qiao <andrewqiao2004@gmail.com>
2022-08-17 12:07:31 -0400

    Connect database to links page

--- components/sections/LinksList.tsx
+++ components/sections/LinksList.tsx
@@ -1,23 +1,38 @@
 import { ReactElement } from 'react';
+import { useState, useEffect } from 'react';
+import { ref, onValue } from 'firebase/database';
+import { db } from '../../utils/firebaseConfig';
 
-const LINKS = [
-  { id: 'link-1', link: 'https://createmarkham.ca/', text: 'Website' },
-  {
-    id: 'link-2',
-    link: 'https://www.instagram.com/createmarkham/',
-    text: 'Instagram',
-  },
-  { id: 'link-3', link: 'mailto:createcouncilorg@gmail.com', text: 'Email' },
-];
+type Link = {
+  id: string;
+  text: string;
+  link: string;
+};
 
 const LinksList = (): ReactElement => {
+  const [links, setLinks] = useState([
+    { id: 'application-1', text: 'Website', link: 'createmarkham.ca' },
+  ]);
+
+  useEffect(() => {
+    const linksRef = ref(db, 'links');
+    onValue(linksRef, (snapshot) => {
+      let data = snapshot.val();
+      let links: Link[] = [];
+      for (let key in data) {
+        links.push(data[key]);
+      }
+      setLinks(links);
+    });
+  }, []);
+
   return (
     <section className="section m-horizontal">
       <div className="text-center">
         <h1 className="h1 mb-6 md:mb-9">links</h1>
       </div>
       <div className="mx-auto max-w-[500px]">
-        {LINKS.map((link) => (
+        {links.map((link) => (
           <Link key={link.id} text={link.text} link={link.link} />
         ))}
       </div>
