82789b22870464ef02c85f2c6967655471ebce91
Andrew Qiao <andrewqiao2004@gmail.com>
2022-08-16 22:21:51 -0400

    Comment out gallery page

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -10,7 +10,7 @@ const ROUTES = [
   { id: 'route-1', text: 'Home', route: '/' },
   { id: 'route-2', text: 'Applications', route: '/applications' },
   { id: 'route-3', text: 'Team', route: '/team' },
-  { id: 'route-4', text: 'Gallery', route: '/gallery' },
+  // { id: 'route-4', text: 'Gallery', route: '/gallery' },
 ];
 
 interface HeaderPropsInterface {
