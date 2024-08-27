8b9941fb0d1201c4affb39e7eeab530c1b54c53e
t1ffanyh <Tiffanyho0903@gmail.com>
2024-08-27 15:27:08 -0400

    hub

--- components/sections/MyCreateStory.tsx
+++ components/sections/MyCreateStory.tsx
@@ -5,6 +5,7 @@ import landingGraphic from '../../public/img/graphics/landing.png';
 import brain from '../../public/img/sponsers/brain.png';
 import sticker from '../../public/img/sponsers/sticker.png';
 import hub from '../../public/img/sponsers/hublink.png';
+import hublink from '../../public/img/sponsers/hublink.png';
 import markham from '../../public/img/sponsers/markham.png';
 import creativegenius from '../../public/img/sponsers/creativegenius.png';
 
@@ -22,7 +23,7 @@ const MyCreateStory = (): ReactElement => {
         height="183px"
       />
       <Image
-        src={hub}
+        src={hublink}
         alt="Hub Climbing – Rock Climbing and Birthday Parties in Markham"
         width="199px"
         height="93px"
