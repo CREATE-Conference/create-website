9e63cfb818a8c82e6d68c284f87e2ee55e7047a9
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-02 18:27:19 -0400

    gallery

--- components/sections/GalleryPage.tsx
+++ components/sections/GalleryPage.tsx
@@ -1,11 +1,19 @@
 import { ReactElement, useState } from 'react';
 import Button from '../UI/Button';
+import { GALLERY_IMG } from '../../constants/constants';
 
 const GalleryPage = (): ReactElement => {
   return (
     <section className="section center items-center">
       <div className="lg:-mt-240 text-center">
         <h1 className="h1 mb-5 xs:mb-10 md:mb-14 lg:mb-14 lg:mt-10">gallery</h1>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+          {GALLERY_IMG.map((src, index) => (
+            <div key={index} className="gallery-item">
+              <img src={src} alt={`Gallery Image ${index + 1}`} className="w-full h-auto object-cover" />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- constants/constants.js
+++ constants/constants.js
@@ -661,4 +661,57 @@ export const FOOTER_SOCIALS = [
   },
 ];
 
+//GALLERY 2023 PICS
+import gallery1 from 'public/img/gallery/4971_0001.jpg';
+import gallery2 from 'public/img/gallery/4971_0002.jpg';
+import gallery3 from 'public/img/gallery/4971_0004.jpg';
+import gallery4 from 'public/img/gallery/4971_0005.jpg';
+import gallery5 from 'public/img/gallery/4971_0006.jpg';
+import gallery6 from 'public/img/gallery/4971_0008.jpg';
+import gallery7 from 'public/img/gallery/4971_0009.jpg';
+import gallery8 from 'public/img/gallery/4971_0011.jpg';
+import gallery9 from 'public/img/gallery/4971_0015.jpg';
+import gallery10 from 'public/img/gallery/4971_0016.jpg';
+import gallery11 from 'public/img/gallery/4971_0017.jpg';
+import gallery12 from 'public/img/gallery/4971_0018.jpg';
+import gallery13 from 'public/img/gallery/4971_0020.jpg';
+import gallery14 from 'public/img/gallery/4971_0021.jpg';
+import gallery15 from 'public/img/gallery/4971_0022.jpg';
+import gallery16 from 'public/img/gallery/4971_0023.jpg';
+import gallery17 from 'public/img/gallery/4971_0024.jpg';
+import gallery18 from 'public/img/gallery/4971_0025.jpg';
+import gallery19 from 'public/img/gallery/4971_0026.jpg';
+import gallery20 from 'public/img/gallery/4971_0027.jpg';
+import gallery21 from 'public/img/gallery/4971_0028.jpg';
+import gallery22 from 'public/img/gallery/4971_0029.jpg';
+import gallery23 from 'public/img/gallery/4971_0031.jpg';
+import gallery24 from 'public/img/gallery/4971_0032.jpg';
+import gallery25 from 'public/img/gallery/4971_0033.jpg';
 
+export const GALLERY_IMG = [
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  gallery6,
+  gallery7,
+  gallery8,
+  gallery9,
+  gallery10,
+  gallery11,
+  gallery12,
+  gallery13,
+  gallery14,
+  gallery15,
+  gallery16,
+  gallery17,
+  gallery18,
+  gallery19,
+  gallery20,
+  gallery21,
+  gallery22,
+  gallery23,
+  gallery24,
+  gallery25,
+];
