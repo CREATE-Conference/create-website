31aa97659987afa523d58180537faac200f3b9d0
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-03 12:25:31 -0400

    error

--- components/sections/GalleryPage.tsx
+++ components/sections/GalleryPage.tsx
@@ -1,47 +1,47 @@
 import { ReactElement, useState } from 'react';
 import Button from '../UI/Button';
-import Image from 'next/image';
+// import Image from 'next/image';
 
-import gallery1 from '../public/img/gallery/4971_0001.jpg';
-import gallery2 from '../public/img/gallery/4971_0002.jpg';
-import gallery3 from '../public/img/gallery/4971_0004.jpg';
-import gallery4 from '../public/img/gallery/4971_0005.jpg';
-import gallery5 from '../public/img/gallery/4971_0006.jpg';
-import gallery6 from '../public/img/gallery/4971_0008.jpg';
-import gallery7 from '../public/img/gallery/4971_0009.jpg';
-import gallery8 from '../public/img/gallery/4971_0011.jpg';
-import gallery9 from '../public/img/gallery/4971_0015.jpg';
-import gallery10 from '../public/img/gallery/4971_0016.jpg';
-import gallery11 from '../public/img/gallery/4971_0017.jpg';
-import gallery12 from '../public/img/gallery/4971_0018.jpg';
-import gallery13 from '../public/img/gallery/4971_0020.jpg';
-import gallery14 from '../public/img/gallery/4971_0021.jpg';
-import gallery15 from '../public/img/gallery/4971_0022.jpg';
-import gallery16 from '../public/img/gallery/4971_0023.jpg';
-import gallery17 from '../public/img/gallery/4971_0024.jpg';
-import gallery18 from '../public/img/gallery/4971_0025.jpg';
-import gallery19 from '../public/img/gallery/4971_0026.jpg';
-import gallery20 from '../public/img/gallery/4971_0027.jpg';
-import gallery21 from '../public/img/gallery/4971_0028.jpg';
-import gallery22 from '../public/img/gallery/4971_0029.jpg';
-import gallery23 from '../public/img/gallery/4971_0031.jpg';
-import gallery24 from '../public/img/gallery/4971_0032.jpg';
-import gallery25 from '../public/img/gallery/4971_0033.jpg';
+// import gallery1 from '../public/img/gallery/4971_0001.jpg';
+// import gallery2 from '../public/img/gallery/4971_0002.jpg';
+// import gallery3 from '../public/img/gallery/4971_0004.jpg';
+// import gallery4 from '../public/img/gallery/4971_0005.jpg';
+// import gallery5 from '../public/img/gallery/4971_0006.jpg';
+// import gallery6 from '../public/img/gallery/4971_0008.jpg';
+// import gallery7 from '../public/img/gallery/4971_0009.jpg';
+// import gallery8 from '../public/img/gallery/4971_0011.jpg';
+// import gallery9 from '../public/img/gallery/4971_0015.jpg';
+// import gallery10 from '../public/img/gallery/4971_0016.jpg';
+// import gallery11 from '../public/img/gallery/4971_0017.jpg';
+// import gallery12 from '../public/img/gallery/4971_0018.jpg';
+// import gallery13 from '../public/img/gallery/4971_0020.jpg';
+// import gallery14 from '../public/img/gallery/4971_0021.jpg';
+// import gallery15 from '../public/img/gallery/4971_0022.jpg';
+// import gallery16 from '../public/img/gallery/4971_0023.jpg';
+// import gallery17 from '../public/img/gallery/4971_0024.jpg';
+// import gallery18 from '../public/img/gallery/4971_0025.jpg';
+// import gallery19 from '../public/img/gallery/4971_0026.jpg';
+// import gallery20 from '../public/img/gallery/4971_0027.jpg';
+// import gallery21 from '../public/img/gallery/4971_0028.jpg';
+// import gallery22 from '../public/img/gallery/4971_0029.jpg';
+// import gallery23 from '../public/img/gallery/4971_0031.jpg';
+// import gallery24 from '../public/img/gallery/4971_0032.jpg';
+// import gallery25 from '../public/img/gallery/4971_0033.jpg';
 
 const GalleryPage = (): ReactElement => {
   return (
     <section className="section center items-center">
       <div className="lg:-mt-240 text-center">
-        <h1 className="h1 mb-5 xs:mb-10 md:mb-14 lg:mb-14 lg:mt-10">gallery</h1>
+        <h1 className="h1 mb-5 xs:mb-10 md:mb-14 lg:mb-14 lg:mt-10">Gallery</h1>
       </div>
-      <div className="absolute -top-4 -left-5 hidden xl:block">
+      {/* <div className="absolute -top-4 -left-5 hidden xl:block">
         <Image
           src={gallery1}
           alt="Gallery1"
           width="183px"
           height="183px"
         />
-      </div>
+      </div> */}
     </section>
   );
 };
