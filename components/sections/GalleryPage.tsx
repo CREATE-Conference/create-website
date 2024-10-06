f46d36353757df2220a61f2525d04100331ebce5
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-05 20:00:10 -0400

    space

--- components/sections/GalleryPage.tsx
+++ components/sections/GalleryPage.tsx
@@ -33,7 +33,7 @@ const GalleryPage = (): ReactElement => {
       <div className="lg:-mt-240 text-center">
         <h1 className="h1 mb-5 xs:mb-10 md:mb-14 lg:mb-14 lg:mt-10">Gallery</h1>
       </div>
-      <div className="absolute -top-4 -left-5 hidden xl:block">
+      <div className="absolute -top-4 -left-15 hidden xl:block">
         <Image
           src={gallery1}
           alt="IMG1"
