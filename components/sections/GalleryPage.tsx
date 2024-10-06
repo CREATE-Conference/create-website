7bcf1f6ef6d89d2082a79688ac15313ee6d05cd7
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-05 22:08:39 -0400

    remove sideways and wide pics

--- components/sections/GalleryPage.tsx
+++ components/sections/GalleryPage.tsx
@@ -1,5 +1,4 @@
 import { ReactElement, useState } from 'react';
-import Button from '../UI/Button';
 import Image from 'next/image';
 import gallery1 from '../../public/img/gallery/4971_0001.png';
 import gallery2 from '../../public/img/gallery/4971_0002.png';
@@ -18,7 +17,7 @@ import gallery14 from '../../public/img/gallery/4971_0021.png';
 import gallery15 from '../../public/img/gallery/4971_0022.png';
 import gallery16 from '../../public/img/gallery/4971_0023.png';
 import gallery17 from '../../public/img/gallery/4971_0024.png';
-import gallery18 from '../../public/img/gallery/4971_0025.png';
+//import gallery18 from '../../public/img/gallery/4971_0025.png';
 import gallery19 from '../../public/img/gallery/4971_0026.png';
 import gallery20 from '../../public/img/gallery/4971_0027.png';
 import gallery21 from '../../public/img/gallery/4971_0028.png';
@@ -37,7 +36,7 @@ import gallery33 from '../../public/img/gallery/IMG_2171.png';
 //import gallery34 from '../../public/img/gallery/IMG_5681.png';
 //import gallery35 from '../../public/img/gallery/IMG_5682.png';
 //import gallery36 from '../../public/img/gallery/IMG_5697.png';
-import gallery37 from '../../public/img/gallery/IMG_5724.png';
+//import gallery37 from '../../public/img/gallery/IMG_5724.png';
 import gallery38 from '../../public/img/gallery/IMG_5726.png';
 //import gallery39 from '../../public/img/gallery/IMG_6399.png';
 //import gallery40 from '../../public/img/gallery/IMG_6410.png';
@@ -45,7 +44,7 @@ import gallery38 from '../../public/img/gallery/IMG_5726.png';
 //import gallery42 from '../../public/img/gallery/IMG_6500.png';
 import gallery43 from '../../public/img/gallery/IMG_6581.png';
 import gallery44 from '../../public/img/gallery/IMG_6588.png';
-import gallery45 from '../../public/img/gallery/IMG_8432.png';
+//import gallery45 from '../../public/img/gallery/IMG_8432.png';
 import gallery46 from '../../public/img/gallery/IMG_8463.png';
 import gallery47 from '../../public/img/gallery/IMG_8524.png';
 //import gallery48 from '../../public/img/gallery/IMG_8721.png';
@@ -54,9 +53,9 @@ import gallery47 from '../../public/img/gallery/IMG_8524.png';
 const GALLERY = [
   gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8,
   gallery9, gallery10, gallery11, gallery12, gallery13, gallery14, gallery15,
-  gallery16, gallery17, gallery18, gallery19, gallery20, gallery21, gallery22,
+  gallery16, gallery17, gallery19, gallery20, gallery21, gallery22,
   gallery23, gallery24, gallery25, gallery26, gallery27, gallery28, gallery29,
-  gallery33, gallery37, gallery38, gallery43, gallery44, gallery45, gallery46,
+  gallery33, gallery38, gallery43, gallery44, gallery46,
   gallery47
 ];
 
@@ -83,23 +82,3 @@ const GalleryPage = (): ReactElement => {
 };
 
 export default GalleryPage;
-
-// const GalleryPage = (): ReactElement => {
-//   return (
-//     <section className="section center items-center">
-//       <div className="lg:-mt-240 text-center">
-//         <h1 className="h1 mb-5 xs:mb-10 md:mb-14 lg:mb-14 lg:mt-10">Gallery</h1>
-//       </div>
-//       <div className="absolute -bottom-6 left-8 hidden xl:block">
-//         <Image
-//           src={gallery1}
-//           alt="IMG1"
-//           width="507px"
-//           height="340px"
-//         />
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default GalleryPage;
