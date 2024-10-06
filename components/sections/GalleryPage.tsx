1a3ecc5327c6ede2eba2589f56e96d42a6fa735a
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-05 20:34:15 -0400

    grid

--- components/sections/GalleryPage.tsx
+++ components/sections/GalleryPage.tsx
@@ -59,7 +59,7 @@ const GalleryPage = (): ReactElement => {
   return (
     <section className="section center items-center">
       <div className="lg:-mt-240 text-center">
-        <h1 className="h1 mb-5 xs:mb-10 md:mb-14 lg:mb-14 lg:mt-10">gallery</h1>
+        <h1 className="h1 mb-5 xs:mb-10 md:mb-14 lg:mb-14 lg:mt-10">Gallery</h1>
       </div>
       <div className="absolute -bottom-6 left-8 hidden xl:block">
         <Image
