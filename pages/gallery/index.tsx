11ac26561baaf7ece140a904358f18503775ccaf
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-05 22:34:14 -0400

    SEO gallery

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next';
 import Page from '../../components/layout/Page';
 import GalleryPage from '../../components/sections/GalleryPage';
+import SEO from '../../components/SEO/SEO';
 
 const Gallery: NextPage = () => {
   return (
+    <SEO
+        title="Gallery | CREATE Markham"
+        description="Interested in gaining volunteer hours in Markham? Apply for the CREATE Conference today!"
+        keywords=""
+      />
     <Page curPage="Gallery">
       <GalleryPage />
     </Page>
