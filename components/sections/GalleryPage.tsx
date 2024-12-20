d8341fc054d2f50a34e69f9ecde86ada7dd55aae
tiiffanyho <Tiffanyho0903@gmail.com>
2024-12-19 20:18:58 -0500

    embed link

--- components/sections/GalleryPage.tsx
+++ components/sections/GalleryPage.tsx
@@ -7,6 +7,14 @@ const GalleryPage = (): ReactElement => {
       <div className="lg:-mt-240 text-center">
         <h1 className="h1 mb-5 xs:mb-10 md:mb-14 lg:mb-14 lg:mt-10">gallery</h1>
       </div>
+      <div className="w-full h-full">
+        <iframe
+          src="https://drive.google.com/drive/folders/1b3b7dnTPzxtMABpfR_614fesngDgtAAs"
+          width="100%"
+          height="600px"
+          frameBorder="0"
+        ></iframe>
+      </div>
     </section>
   );
 };
