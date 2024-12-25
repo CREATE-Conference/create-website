30dcc61150df0abce704edc64ebba6934ffd896c
tiiffanyho <Tiffanyho0903@gmail.com>
2024-12-25 12:34:49 -0500

    more imgs

--- components/sections/GalleryPage.tsx
+++ components/sections/GalleryPage.tsx
@@ -18,7 +18,7 @@ import gallery14 from '../../public/img/gallery/4971_0021.png';
 import gallery15 from '../../public/img/gallery/4971_0022.png';
 import gallery16 from '../../public/img/gallery/4971_0023.png';
 import gallery17 from '../../public/img/gallery/4971_0024.png';
-//import gallery18 from '../../public/img/gallery/4971_0025.png';
+import gallery18 from '../../public/img/gallery/4971_0025.png';
 import gallery19 from '../../public/img/gallery/4971_0026.png';
 import gallery20 from '../../public/img/gallery/4971_0027.png';
 import gallery21 from '../../public/img/gallery/4971_0028.png';
@@ -27,7 +27,7 @@ import gallery23 from '../../public/img/gallery/4971_0031.png';
 import gallery24 from '../../public/img/gallery/4971_0032.png';
 import gallery25 from '../../public/img/gallery/4971_0033.png';
 import gallery26 from '../../public/img/gallery/_7EE7388.png';
-//import gallery27 from '../../public/img/gallery/_7EE7438.png';
+import gallery27 from '../../public/img/gallery/_7EE7438.png';
 import gallery28 from '../../public/img/gallery/_7EE7568.png';
 import gallery29 from '../../public/img/gallery/_7EE7688.png';
 //import gallery30 from '../../public/img/gallery/IMG_0216.png';
@@ -54,8 +54,8 @@ import gallery47 from '../../public/img/gallery/IMG_8524.png';
 const GALLERY = [
   gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8,
   gallery9, gallery10, gallery11, gallery12, gallery13, gallery14, gallery15,
-  gallery16, gallery17, gallery19, gallery20, gallery21, gallery22,
-  gallery23, gallery24, gallery25, gallery26, gallery28, gallery29,
+  gallery16, gallery17, gallery18, gallery19, gallery20, gallery21, gallery22,
+  gallery23, gallery24, gallery25, gallery26, gallery27, gallery28, gallery29,
   gallery33, gallery38, gallery43, gallery44, gallery47
 ];
 
