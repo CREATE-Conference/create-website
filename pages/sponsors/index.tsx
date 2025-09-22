6e92d990a0418d3462eb18e8dcf8d00b07d52dc0
Marcus-Yu <marcusyuca@gmail.com>
2025-09-21 20:17:11 -0400

    Add files via upload

--- pages/sponsors/index.tsx
+++ pages/sponsors/index.tsx
@@ -1,1 +1,21 @@
+import type { NextPage } from 'next';
+import Page from '../../components/layout/Page';
+import SponsorsPage from '../../components/sections/SponsorsPage';
+import SEO from '../../components/SEO/SEO';
 
+const Sponsors: NextPage = () => {
+  return (
+    <>
+      <SEO
+        title="Sponsors | CREATE Markham"
+        description="View our sponsors and partners!"
+        keywords=""
+      />
+      <Page curPage="Sponsors">
+        <SponsorsPage />
+      </Page>
+    </>
+  );
+};
+
+export default Sponsors;
