815d7c3eb2887e36ded3735d2b1b77e3d49ba68f
Marcus-Yu <marcusyuca@gmail.com>
2025-09-28 13:40:07 -0400

    Update Header.tsx

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -51,8 +51,8 @@ const HeaderLogo = (): ReactElement => {
       <a className="flex w-[116px] items-center lg:w-[132px]">
         <Image
           src={createLogo}
-          width="132px"
-          height="55px"
+          width="140px"
+          height="36px"
           alt="CREATE Markham logo"
         />
       </a>
