72f6d93af32da4a278c36d2fa301639513cb1f24
Marcus-Yu <marcusyuca@gmail.com>
2026-03-07 22:17:44 -0500

    Refactor image dimensions to numeric values
    Updated image width and height attributes to use numeric values instead of strings.

--- components/sections/MyCreateStory.tsx
+++ components/sections/MyCreateStory.tsx
@@ -22,29 +22,29 @@ const MyCreateStory = (): ReactElement => {
       <Image
         src={brain}
         alt="Brain Power"
-        width="500px"
-        height="183px"
+        width={500}
+        height={183}
       />
       <a href="https://hubclimbing.com/markham" target="_blank" rel="noopener noreferrer">
         <Image
           src={hub}
           alt="Hub Climbing – Rock Climbing and Birthday Parties in Markham"
-          width="259px"
-          height="153px"
+          width={259}
+          height={153}
         />
       </a>
       <Image
         src={markham}
         alt="Markham"
-        width="450px"
-        height="183px"
+        width={450}
+        height={183}
        
       />
       <Image
         src={creativegenius}
         alt="Creative Genius Academy"
-        width="300px"
-        height="220px"
+        width={300}
+        height={220}
       />
       <a href="https://www.stickergiant.com/coupons" target="_blank" rel="noopener noreferrer">
         <Image
