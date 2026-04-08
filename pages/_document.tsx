cb5da8e351b70569f9f7303190fe24dccceeb763
Stephen <stephenit@gmail.com>
2026-04-08 09:35:54 -0400

    crossOrigin=anonymous

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,7 +24,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap"
