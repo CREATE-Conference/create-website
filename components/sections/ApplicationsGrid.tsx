50f29962cd484b0b205965294a336578eba73b2a
judianswyl <brianlin221@gmail.com>
2026-09-01 21:18:10 -0400

    Fixed formatting

--- components/sections/ApplicationsGrid.tsx
+++ components/sections/ApplicationsGrid.tsx
@@ -59,16 +59,16 @@ const ApplicationsGrid = (): ReactElement => {
         </Row2>
       ) : (
         <p className="text-center text-lg font-medium text-green-1">
-                    Are you interested in taking the opportunity to lead and connect with like-minded individuals?
+  Are you interested in taking the opportunity to lead and connect with like-minded individuals?
           Time runs out quickly! Apply to be a Maven before September 11th, 11:59PM EST here at: {" "}
-                      <a
-                      href="https://forms.gle/cUH7EKCkDp7FPkir8"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{textDecoration: "underline", color: "#0645AD"}}
-                      >
-                        https://forms.gle/cUH7EKCkDp7FPkir8
-                    </a>
+          <a
+            href="https://forms.gle/cUH7EKCkDp7FPkir8"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{textDecoration: "underline", color: "#0645AD"}}
+           >
+             https://forms.gle/cUH7EKCkDp7FPkir8
+          </a>
         </p>
       )}
     </section>
